0bcac94e021a985b507e09a49dc8fe181b4a1932
Amaya-2001 <amawickramasinghe2001@gmail.com>
2022-11-13 13:44:02 +0530

    create login page

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+export const Login= () => {
+    return (
+       
+        <div class="container is-widescreen">
+        <div class="column is-half  ">
+            <h1 class="title">Attendy Welcomes You!</h1>
+            <h1 class="subtitle">Let's get you logged in.</h1>
+        
+       <input class="input mb-3 " type="text" placeholder="User ID "></input><br></br>
+       <input class="input mb-3 " type="password" placeholder="Password"></input><br></br>
+       <button class="button is-medium is-fullwidth">Login</button>
+      
+     </div>
+        </div>
+     
+       
+    )
+}
