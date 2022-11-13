0c370689e47643010bb512abb4cbba85d422eca3
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2022-11-13 14:36:45 +0530

    create card basic skeleton

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+export const Card = () => {
+  return (
+    <div>
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+          </figure>
+          <h1>Image comes here</h1>
+        </div>
+        <div class="card-content">
+          <h1>Card content here</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
