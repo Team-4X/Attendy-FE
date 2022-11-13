b91bacc6ee7b6188ebc954f2c83e1d7cc3a0b999
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2022-11-14 00:54:26 +0530

    create a basic card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,26 @@
-export const Card = () => {
+import { useState } from "react";
+
+export const Card = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [attendanceCount, setAttendanceCount] = useState(props.attendanceCount);
+  const [image, setImage] = useState(props.image);
+
   return (
-    <div>
-      <div class="card">
-        <div class="card-image">
-          <figure class="image is-4by3">
-          </figure>
-          <h1>Image comes here</h1>
-        </div>
-        <div class="card-content">
-          <h1>Card content here</h1>
-        </div>
+    <div className="card">
+      <div className="card-image px-0 has-text-centered">
+        <img src={image} alt="placeholder image" />
+      </div>
+      <div className="card-content">
+        <p className="has-text-centered">
+          <a href="" className="has-text-link has-text-weight-bold">{title}</a>
+        </p>
+      </div>
+      <div className="card-footer">
+        <p className="card-footer-item">
+          {attendanceCount}
+        </p>
       </div>
     </div>
   )
 }
+
