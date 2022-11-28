8dc492c58f6e4823853e5a4186bcace3e67b1932
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2022-11-28 15:31:42 +0530

    update side-bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,21 @@
-import "./SideBar.css"
+import "./SideBar.css";
 
 export const SideBar = (props) => {
-
   return (
-    <div className="has-background-light box is-fullheight">
-      <div className="mt-5 pl-5 pt-2 is-size-4"><a>Dashboard</a></div>
-      <div className="mt-5 pl-5 is-size-4"><a>Time Table</a></div>
-      <div className="mt-5 pl-5 is-size-4"><a>Calendar</a></div>
-      <div className="mt-5 pl-5 pb-2 is-size-4"><a>Attendance</a></div>
+    <div className="has-background-light box is-fullheight is-hidden-mobile">
+      <div className="box">
+        <a className="has-text-dark">Dashboard</a>
+      </div>
+
+      <div className="box">
+        <a className="has-text-dark">Time Table</a>
+      </div>
+      <div className="box">
+        <a className="has-text-dark">Calendar</a>
+      </div>
+      <div className="box">
+        <a className="has-text-dark">Attendance</a>
+      </div>
     </div>
   );
 };
