11e2d4225936518ba3aa9909abbd0b43901bd775
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2022-12-02 16:50:21 +0530

    align columns

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,71 +4,71 @@ import "bulma/css/bulma.min.css";
 export const SignUp = () => {
     return (
 
-      <div className="signform">
-        <h1 className="title is-1">WELCOME ATTENDY</h1>
-        <h2 className="title is-1">Let's get you started!</h2>  
-
-        <div class="columns">
+        <div class="container is-max-desktop">
             <div class="column is-half">
-                <label class="label">First Name</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="Text input"/>
-                </div>    
-            </div>
-        </div>
+                <h1 className="title is-1">WELCOME ATTENDY</h1>
+                <h2 className="title is-1">Let's get you started!</h2>  
+                <br></br>
 
-        <div class="field">
-            <label class="label">Last Name</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input"/>
-            </div>
-        </div>
+                <div class="field"> 
+                    <label class="label">First Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>  
+                    </div>
+                </div>
 
-        <div class="field">
-            <label class="label">User ID</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input"/>
-            </div>
-        </div>
+                <div class="field">
+                    <label class="label">Last Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>
+                    </div>
+                </div>
 
-        <div class="field">
-            <label class="label">Last Name</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input"/>
-            </div>
-        </div>
+                <div class="field">
+                    <label class="label">User ID</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>
+                    </div>
+                </div>
 
-        <div class="field">
-            <label class="label">E-mail</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input"/>
-            </div>
-        </div>
+                <div class="field">
+                    <label class="label">Last Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>
+                    </div>
+                </div>
 
-        <div class="field">
-            <label class="label">Password</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input"/>
-            </div>
-        </div>
+                <div class="field">
+                    <label class="label">E-mail</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>
+                    </div>
+                </div>
 
-        <div class="field">
-            <label class="label">Confirm Password</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input"/>
-            </div>
-        </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>
+                    </div>
+                </div>
 
-        <div class="grouped">
-            <div class="control">
-                <button class="button is-link">Sign-up</button>
-            </div>
-            <div class="control">
-                <button class="button is-link is-light">Cancel</button>
-            </div>
-        </div>
+                <div class="field">
+                    <label class="label">Confirm Password</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input"/>
+                    </div>
+                </div>
 
-      </div>  
+                <div class="grouped">
+                    <div class="control">
+                        <button class="button is-link">Sign-up</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-link is-light">Cancel</button>
+                    </div>
+                </div>
+            </div>    
+        </div>  
     )
 }
 
