acd29ce32418f2624c2e099c46876c7c0e686e8f
Amaya-2001 <amawickramasinghe2001@gmail.com>
2022-12-03 10:17:07 +0530

    update login.js file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,4 @@
+import "./login-content.css";
 export const Login = () => {
   return (
     <div class="container mt-5 is-max-desktop">
@@ -6,27 +7,33 @@ export const Login = () => {
           <h1 class="title has-text-centered">Attendy Welcomes You!</h1>
 
           <h1 class="subtitle  has-text-centered">Let's get you logged in.</h1>
-          <br></br>
-          <figure class="image is-2by1">
-            <img src="https://bulma.io/images/placeholders/256x256.png" />
-          </figure>
-          <br></br>
 
-          <input class="input mb-3 " type="text" placeholder="User ID "></input>
-          <br></br>
-          <input
-            class="input mb-3 "
-            type="password"
-            placeholder="Password"
-          ></input>
-          <br></br>
-          <button class="button is-medium is-fullwidth is-primary">
-            Login
-          </button>
-          <br></br>
-          <span>
-            <a href="#">Create an account?</a>
-          </span>
+          <div class="has-text-centered">
+            <img
+              src="3255337.jpg"
+              alt="login"
+              className="image-size"
+              width="350px"
+            />
+
+            <input
+              class="input mb-3 "
+              type="text"
+              placeholder="User ID "
+            ></input>
+            <br></br>
+            <input
+              class="input mb-3 "
+              type="password"
+              placeholder="Password"
+            ></input>
+            <br></br>
+            <button class="button is-dark">Login</button>
+            <br></br>
+            <span>
+              <a href="#">Create an account?</a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
