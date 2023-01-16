a25058bc029ae345bd9cb834678d493dff13948c
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-01-16 12:18:20 +0530

    fixed cancel button in signup.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,87 @@
 import "./login-content.css";
 import { Link } from "react-router-dom";
 import image from "../assets/working-together.jpg";
+import { useState } from "react";
 
 export const Login = () => {
-  return (
-    <div class="container mt-5 is-max-desktop">
-      <div class="columns is-centered">
-        <div class="column is-half  ">
-          <h1 class="title has-text-centered">Attendy Welcomes You!</h1>
+    const [data, setData] = useState();
+    const [name, setName] = useState();
 
-          <h1 class="subtitle  has-text-centered">Let's get you logged in.</h1>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        fetch('http://localhost:4000/test')
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                setData(json);
+                console.log(json[0]);
+                setName(json[0].name);
+            });
+    }
+    // const handleSubmit = async (e) => {
+    //   e.preventDefault();
+    //   const data = new FormData(e.currentTarget);
+    //   const form = {
+    //     userID: data.get('userID'),
+    //     password: data.get('password'),
+    //   };
+    //   console.log('runs login post');
+    //   const res = await fetch('http://localhost:4000/auth/login', {
+    //     method: "POST",
+    //     body: JSON.stringify(form),
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //   })
+    //   console.log('runs login post');
+    //   console.log(res);
+    // };
 
-          <div class="has-text-centered">
-            <img
-              // src="3255337.jpg"
-              src={image}
-              alt="login"
-              className="image-size"
-              width="350px"
-            />
+    return (
+        <div class="container mt-5 is-max-desktop">
+            <div class="columns is-centered">
+                <div class="column is-half  ">
+                    <h1 class="title has-text-centered">Attendy Welcomes You!</h1>
 
-            <input
-              class="input mb-3 "
-              type="text"
-              placeholder="User ID "
-            ></input>
-            <br></br>
-            <input
-              class="input mb-3 "
-              type="password"
-              placeholder="Password"
-            ></input>
-            <br></br>
-            <button class="button is-dark">Login</button>
-            <br></br>
-            <span>
-              <a href="#">
-                <Link to={"/auth/register"}>Create an account?</Link>
-              </a>
-            </span>
-          </div>
+                    <h1 class="subtitle  has-text-centered">Let's get you logged in.</h1>
+                    <h1>{name}</h1>
+
+                    <div class="has-text-centered">
+                        <img
+                            // src="3255337.jpg"
+                            src={image}
+                            alt="login"
+                            className="image-size"
+                            width="350px"
+                        />
+
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                class="input mb-3 "
+                                type="text"
+                                placeholder="User ID "
+                                name="userID"
+                            ></input>
+                            <br></br>
+                            <input
+                                class="input mb-3 "
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                            ></input>
+                            <br></br>
+                            <button class="button is-dark">Login</button>
+                            <br></br>
+                            <span>
+                                <a href="#">
+                                    <Link to={"/auth/register"}>Create an account?</Link>
+                                </a>
+                            </span>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,8 +2,47 @@ import React from "react";
 import "bulma/css/bulma.min.css";
 import image from "./14230944_5437683.jpg";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
 
 export const SignUp = () => {
+  // const [form, setForm] = useState({
+  //   firstName: '',
+  //   lastName: '',
+  //   userID: '',
+  //   email: '',
+  //   password: '' 
+  // })
+  //
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    const form = {
+      name: data.get('firstName') + ' ' + data.get('lastName'),
+      userID: data.get('userID'),
+      email: data.get('email'),
+      password: data.get('password')
+    };
+    const res = await fetch('http://localhost:4000/auth/register', {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    console.log(res);
+  }
+
+  const handleCancel = () => {
+    navigate('/');
+  }
+
+  // const handleInput = (e) => {
+  //   setForm({ ...form, [e.target.name]: e.target.value });
+  // }
+
   return (
     <div class="container mt-5 is-max-desktop">
       <div class="columns is-centered">
@@ -16,58 +55,63 @@ export const SignUp = () => {
           <div class="has-text-centered">
             <img src={image} alt="signUp" className="image is-4by2" />
 
-            <div class="field mb-4">
-              <div class="control">
-                <input class="input" type="text" placeholder="First Name" />
+            <form onSubmit={handleSubmit}>
+              <div class="field mb-4">
+                <div class="control">
+                  <input class="input" name="firstName" type="text" placeholder="First Name" required={true} />
+                </div>
               </div>
-            </div>
 
-            <div class="field mb-4">
-              <div class="control">
-                <input class="input" type="text" placeholder="Last Name" />
+              <div class="field mb-4">
+                <div class="control">
+                  <input class="input" name="lastName" type="text" placeholder="Last Name" required={true} />
+                </div>
               </div>
-            </div>
 
-            <div class="field mb-4">
-              <div class="control">
-                <input class="input" type="text" placeholder="User ID" />
+              <div class="field mb-4">
+                <div class="control">
+                  <input class="input" name="userID" type="text" placeholder="User ID" required={true} />
+                </div>
               </div>
-            </div>
 
-            <div class="field mb-4">
-              <div class="control">
-                <input class="input" type="text" placeholder="E-mail" />
+              <div class="field mb-4">
+                <div class="control">
+                  <input class="input" name="email" type="text" placeholder="E-mail" required={true} />
+                </div>
               </div>
-            </div>
 
-            <div class="field mb-4">
-              <div class="control">
-                <input class="input" type="text" placeholder="Password" />
+              <div class="field mb-4">
+                <div class="control">
+                  <input class="input" name="password" type="password" placeholder="Password" required={true} />
+                </div>
               </div>
-            </div>
 
-            <div class="field mb-4">
-              <div class="control">
-                <input
-                  class="input"
-                  type="text"
-                  placeholder="Confirm Password"
-                />
+              <div class="field mb-4">
+                <div class="control">
+                  <input
+                    class="input"
+                    type="password"
+
+                    placeholder="Confirm Password"
+                    required={true}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div class="grouped">
+              <div class="grouped">
+                <div class="control">
+                  <button class="button is-dark is-fullwidth mb-2" type="submit">
+                    Sign-up
+                  </button>
+                </div>
+
+              </div>
               <div class="control">
-                <button class="button is-dark is-fullwidth mb-2">
-                  Sign-up
+                <button onClick={handleCancel} type="button" class="button is-link is-fullwidth mb-5">
+                  <Link to={"/login"}>Cancel</Link>
                 </button>
               </div>
-              <div class="control">
-                <button class="button is-link is-light is-fullwidth mb-5">
-                  <Link to={"/"}>Cancel</Link>
-                </button>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
