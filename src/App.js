8237ba06500e8355c38d66ee940ed4319c8439fd
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-01-22 12:34:46 +0530

    create protected routes, need to make them redirect to loginpage if not logged in

--- src/App.js
+++ src/App.js
@@ -8,25 +8,36 @@ import { StudentManage } from "./components/StudentManage";
 import { Staff } from "./components/Staff";
 import { StaffManage } from "./components/StaffManage";
 import { MarkAttendance } from "./components/MarkAttendance";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "bulma/css/bulma.min.css";
 
 function App() {
+  // const [token, setToken] = useState(null);
+
+  // const navigate = useNavigate();
+
+  const token = Cookies.get('token');
+
+  console.log(token);
+
   return (
     <div className="App">
       {/* for testing */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/auth/login" element={<Login></Login>}></Route>
           <Route path="/auth/register" element={<SignUp></SignUp>}></Route>
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/contact" element={<ContactUs></ContactUs>}></Route>
           <Route path="/student" element={<Student></Student>}></Route>
-          <Route path="/student-manage" element={<StudentManage></StudentManage>}></Route>
-          <Route path="/staff-manage" element={<StaffManage></StaffManage>}></Route>
+          <Route path="/student-manage" element={token ? <StudentManage/> : <Login/>}></Route>
+          <Route path="/staff-manage" element={token ? <StaffManage/> : <Login />}></Route>
           <Route path="/staff" element={<Staff></Staff>}></Route>
           <Route path="/mark-attendance" element={<MarkAttendance></MarkAttendance>}></Route>
         </Routes>
