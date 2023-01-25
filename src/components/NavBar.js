cf7925a6ab5b5330cc09f79e925e2e6d6b49b3df
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-01-25 17:47:44 +0530

    sus

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,29 @@ import "./assets/navbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Cookies from 'js-cookie';
+import { useDispatch } from "react-redux";
+import {logout} from "../store/auth.js";
+import { useSelector } from 'react-redux';
 
 export const NavBar = () => {
   const navigate = useNavigate();
 
   const [token, setToken] = useState(null);
+  const dispatch = useDispatch(logout());
 
   let buttons;
   const renderButtons = () => {
     if (token != null) {
-      buttons = <div class="buttons"><a className="button is-dark" onClick={logout}><strong><Link to={"/"}>Logout</Link></strong></a></div>
+      buttons = <div class="buttons"><a className="button is-dark" onClick={handleLogout}><strong>Logout</strong></a></div>
     } else {
       buttons = <div className="buttons"><a class="button is-white"><strong><Link to={"/auth/register"}><span id="signup-button">Sign up</span></Link></strong></a><a class="button is-dark"><Link to={"/auth/login"}>Log in</Link></a></div>
     } 
   }
+    
+  // const auth = useSelector((state) => state.auth);
+  // console.log(auth);
 
-  const logout = (e) => {
+  const handleLogout = (e) => {
     e.preventDefault();
     Cookies.remove('token');
     getToken();
