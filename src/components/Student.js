eee4793778bab07eab2577f38126487731cf50b7
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-03-23 09:18:59 +0530

    display attendance details as a table

--- src/components/Student.js
+++ src/components/Student.js
@@ -10,13 +10,20 @@ import "react-calendar/dist/Calendar.css";
 
 export const Student = () => {
   const [studentId, setStudentId] = useState("");
+  const [date, setDate] = useState([]);
+  const [attendanceStatus, setAttendanceStataus] = useState("");
+  const [attendanceDetails, setAttendanceDetails] = useState([]);
 
   const handleFilterClick = () => {
     axios
-      .get(`http://localhost:4000/attendance/${studentId}`)
+      .get(`http://localhost:4000/v1/attendance/${studentId}`)
       .then((res) => {
         // handle successful response
         console.log(res.data);
+
+        const attendanceDetails = res.data;
+        setAttendanceDetails(attendanceDetails);
+        console.log(attendanceDetails);
       })
       .catch((error) => {
         // handle error
@@ -56,7 +63,28 @@ export const Student = () => {
           <button className="button is-dark">Download Report</button>
         </div>
       </div>
-
+      <div className="attedanceTable">
+        <table className="table mt-5 is-bordered is-striped  is-narrow is-hoverable is-max-desktop">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Student ID</th>
+              <th scope="col">Date</th>
+              <th scope="col">Attendance Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {attendanceDetails.map((attendance, index) => (
+              <tr key={attendance._id}>
+                <th scope="row">{index + 1}</th>
+                <td>{attendance.studentID}</td>
+                <td>{attendance.date}</td>
+                <td>{attendance.attendance}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <Footer></Footer>
     </div>
   );
