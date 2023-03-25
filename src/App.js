36d5f28625b6edfebae079f2b0441aab57d62f46
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2023-03-25 11:31:43 +0530

    Create the Teacher Attendance

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Heading } from "./components/Heading";
 import { Card } from "./components/Card";
 import { Footer } from "./components/Footer";
 import { SideBar } from "./components/SideBar";
+import { StudentManage } from "./components/StudentManage";
+import { TeacherInfo } from "./components/TeacherInfo";
 
 import studentImg from "./assets/student.jpg";
 import staffImg from "./assets/staff.jpg";
@@ -11,58 +13,7 @@ import "bulma/css/bulma.min.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* for testing */}
-
-      <div className="columns">
-        <div className="column mt-2 ml-5">
-          <h2 className="title is-2 has-text-success">Navbar here</h2>
-          <Heading headingString="heading is here"></Heading>
-          <hr></hr>
-        </div>
-      </div>
-
-      <div className="columns">
-        <div className="column is-one-third">
-          <SideBar></SideBar>
-        </div>
-        <div className="column">
-          {/* cards should be here */}
-
-          <div className="columns mr-1">
-            <div className="column">
-              <Card
-                title="Students"
-                attendanceCount="300"
-                image={workingImg}
-              ></Card>
-            </div>
-            <div className="column">
-              <Card
-                title="Staff Members"
-                attendanceCount="20"
-                image={staffImg}
-              ></Card>
-            </div>
-          </div>
-
-          <div className="columns mr-1">
-            <div className="column">
-              <Card image={workingImg} title="testTitle" attendanceCount="200"></Card>
-            </div>
-            <div className="column">
-              <Card image={staffImg} title="testTitle" attendanceCount="33"></Card>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="columns">
-        <div className="column">
-          <Footer></Footer>
-        </div>
-      </div>
-    </div>
+    <div><TeacherInfo></TeacherInfo></div>
   );
 }
 
